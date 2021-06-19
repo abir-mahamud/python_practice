cee1e25cc4bdb85d425185fb5771890137cd161c
Abir Mahamud <mr.abirmahamud@gmail.com>
2021-06-20 00:41:07 +0600

    if else

--- if_else/if_else.py
+++ if_else/if_else.py
@@ -4,3 +4,11 @@ favourite = ["mango", "kathal"]
 fruits.extend(favourite)
 print(fruits)
 
+# if else
+age = 40
+if age > 10:
+    print("maybe teenager")
+    if age > 30:
+        print("No she is a women ")
+    else:
+        print("SHIT! buira bedi")
