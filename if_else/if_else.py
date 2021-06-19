8463cfc2d2a05a3b5a32fabc051c9d6a6121baaf
Abir Mahamud <mr.abirmahamud@gmail.com>
2021-06-20 01:18:00 +0600

    if else

--- if_else/if_else.py
+++ if_else/if_else.py
@@ -12,3 +12,23 @@ if age > 10:
         print("No she is a women ")
     else:
         print("SHIT! buira bedi")
+
+# for loop
+names = ["ena", "mena", "tina", "dina"]
+for i in names:
+    if i == "tina":
+        break
+    print(i)
+
+names = ["ena", "mena", "tina", "dina"]
+for i in names:
+    if i == "tina":
+        continue
+    print(i)
+
+
+names = ["ena", "mena", "tina", "dina"]
+work = ["khaiso?", "ghumaiso?"]
+for i in names:
+    for j in work:
+        print(i, j)
