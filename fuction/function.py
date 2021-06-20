beac891cc629d54ef6c58563e4abac4344700454
Abir Mahamud <mr.abirmahamud@gmail.com>
2021-06-21 01:41:48 +0600

    function

---
+++ fuction/function.py
@@ -0,0 +1,15 @@
+# function
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+
+def function(food):
+    for i in food:
+        print(i)
+
+fruits = ["aam", "jaam", "kola"]
+function(fruits)
