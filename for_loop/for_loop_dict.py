fe3e3ee699d259fc143eceed47f7cef5aba9a246
Abir Mahamud <mr.abirmahamud@gmail.com>
2021-06-21 01:24:53 +0600

    dict with for loop

--- for_loop/for_loop_dict.py
+++ for_loop/for_loop_dict.py
@@ -12,3 +12,57 @@ dic = {
 }
 >>>>>>> f05d6a914b3846cfefa9aa7c07e0a4965d0d9322
 print(dic["name"])
+
+
+dic = {
+	"name": "tina",
+	"age":21,
+	"status":"single",
+}
+
+x = dic.keys()
+print(x)
+dic["color"] = "red"
+print(x)
+
+
+dic = {
+	"name": "tina",
+	"age":21,
+	"status":"single",
+}
+dic["name"] = "mena"
+print(dic)
+
+dic.update({"age":25})
+print(dic)
+
+
+dic = {
+	"name": "tina",
+	"age":21,
+	"status":"single",
+}
+
+for i in dic:
+	print(dic[i])
+
+
+
+dic = {
+	"name": "tina",
+	"age":21,
+	"status":"single",
+}
+
+for i in dic.values():
+	print(i)
+
+dic = {
+	"name": "tina",
+	"age":21,
+	"status":"single",
+}
+
+for i in dic.keys():
+	print(i)
